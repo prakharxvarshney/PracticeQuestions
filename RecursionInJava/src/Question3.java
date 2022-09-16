f9d38dcae18a0db8e4d36c740e50457a43e971e4
prakharxvarshney <prakhar.varshney.200004@gmail.com>
2022-09-16 21:37:02 +0530

    Print the fibonacci sequence till nth term

--- RecursionInJava/src/Question3.java
+++ RecursionInJava/src/Question3.java
@@ -1,11 +1,15 @@
-import java.util.*;
 public class Question3 {
-    // Print the fibonacci sequence till nth term
-    Scanner scan = new Scanner(System.in);
-    public static void printFibonacciSequence() {
-
+    public static void printFibonacciSequence(int a, int b, int n) {
+        if(n==2) {
+            return;
+        }
+        int c = a+b;
+        System.out.println(c);
+        printFibonacciSequence(b,c,n-1);
     }
     public static void main(String[] args) {
-
+        System.out.println(0);
+        System.out.println(1);
+        printFibonacciSequence(0,1,10);
     }
 }

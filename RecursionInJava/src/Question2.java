efad900e5610e222c3762c8b56ad5651bef1d19a
prakharxvarshney <prakhar.varshney.200004@gmail.com>
2022-09-16 21:18:27 +0530

    Code is done

--- RecursionInJava/src/Question2.java
+++ RecursionInJava/src/Question2.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Question2 {
     //Print Factorial of a number n
+    static Scanner scan = new Scanner(System.in);
+    public static void printFactorial(int i, int n, int factorial) {
+        if(i==n) {
+            factorial*=i;
+            System.out.println(factorial);
+            return;
+        }
+        factorial = factorial * i;
+        printFactorial(i+1,n,factorial);
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter the number - ");
+        int n=scan.nextInt();
+        int i = 1;
+        int factorial = 1;
+        printFactorial(i,n,factorial);
+    }
 }
